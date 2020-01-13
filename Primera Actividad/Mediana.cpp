0adbf3675b4910ab31a4dea4148c653cd102c5ef
jaritinu <oscar.arp@hotmail.com>
2020-01-12 23:35:50 -0600

    Modify: Mediana.cpp

--- Primera Actividad/Mediana.cpp
+++ Primera Actividad/Mediana.cpp
@@ -5,18 +5,26 @@ float mediana (int *p, int size);
 
 int main (){
     int n=0;
+    int prueba [5]={1,2,3,4,5};
     cout<<"Programa para calcular la mediana"<<endl;
     cout<<"Digite el numero de numeros a ingresar"<<endl;
     cin>>n;
-    
+    int vector[n];
+    cout<<"Ingrese los numeros en orden "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<"Digite un numero: "<<endl;
+        cin>>vector[i];
+    }
+    cout<<"La mediana es: "<<mediana(vector,n)<<endl;
     return 0;
 }
 
 float mediana (int *p, int size){
     float mediana;
-    for(int i=0;i<n;i++){
-        cout<<"Digite un numero: "<<endl;
-        cin>>
+    if(size%2!=0){//caso impar
+        mediana=*(p+(size/2));
+    }else{//caso par
+        mediana=(float (*(p+(size/2)))+float(*(p+((size/2)-1))))/2;
     }
     return mediana;
 }
