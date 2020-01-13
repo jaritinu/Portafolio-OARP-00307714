f5f3eb26182a2b912d2610b0ef71203fe53cb3a8
jaritinu <oscar.arp@hotmail.com>
2020-01-12 22:29:44 -0600

    Modify: Mediana.cpp

---
+++ Primera Actividad/Mediana.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+float mediana (int *p, int size);
+
+int main (){
+    int n=0;
+    cout<<"Programa para calcular la mediana"<<endl;
+    cout<<"Digite el numero de numeros a ingresar"<<endl;
+    cin>>n;
+    
+    return 0;
+}
+
+float mediana (int *p, int size){
+    float mediana;
+    for(int i=0;i<n;i++){
+        cout<<"Digite un numero: "<<endl;
+        cin>>
+    }
+    return mediana;
+}
